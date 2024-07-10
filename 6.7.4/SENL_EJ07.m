42fa075f10ff957eb93a5b25ce582bf0bb33cbe2
Zerker-TF <84744670+Zerker-TF@users.noreply.github.com>
2024-07-10 12:46:27 -0300

    Add files via upload

---
+++ 6.7.4/SENL_EJ07.m
@@ -0,0 +1,56 @@
+%{
+    Determine analiticamente las raices de cada una de las siguientes
+    SENLs:
+
+   
+a)
+
+f1(x,y) = 2x + y - 6
+f2(x,y) = x + 2y
+
+2x + y = 6  (R1)
+ x + 2y = 0  (R2)
+
+Hago R2 - R1 = (x - 2x) + (2y - y) = (0 - 6)
+                3y = -6
+                 y = -2;
+
+Sustituyo y = -2 en R1
+              2x + (-2) = 6
+               x = 4;
+
+Por lo tanto las raices de este SENLs son (x,y) = (4,-2)
+
+b)
+
+f1(x,y) = 3x^2 + 2y - 4
+f2(x,y) = 2x + 2y - 3
+
+3x^2+ 0x + 2y = 4 (R1)
+      2x + 2y = 3 (R2)
+
+Resuelvo R2 para despejar y
+            2x + 2y = 3
+            x + y = 3/2
+                y = 3/2 - x;
+
+Reemplazo y en R1
+            3x^2 + 2y = 4 
+            3x^2 + 2(3/2 - x) = 4
+            3x^2 + 3 - 2x = 4
+            3x^2 - 2x - 1 = 0  
+
+Aplicamos resolvente y obtengo x = 1 y x = -1/3 como raices
+
+    x=1    ==> y = 3/2 - 1 = 1/2
+    x=-1/3 ==> y = 3/2 + 1/3 = 11/6
+
+Por lo tanto las raices del sistema son
+        (x,y) = (1, 1/2)
+        (x,y) = (-1/3, 11/6)
+
+
+ 
+
+
+%}
