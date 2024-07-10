3601267e8a8714b44e5ceaf4b97fa905f7a270cd
Zerker-TF <84744670+Zerker-TF@users.noreply.github.com>
2024-07-10 12:26:46 -0300

    Add files via upload

---
+++ 6.7.4/SENL_EJ06.m
@@ -0,0 +1,38 @@
+%{
+    Determine analiticamente los puntos fijos de cada una de las siguientes
+    generatrices:
+
+    x = g1(x,y)
+    y = g2(x,y)
+a)
+ g1(x,y) = x - y^2
+ g2(x,y) = -x + 6y
+
+g1:
+    x = x - y^2
+    y^2 = 0 => y = 0
+g2:
+    y = -x + 6y (de g1 obtengo y = 0)
+    0 = -x + 0
+    x = 0
+
+Por lo tanto el punto fijo para estas generatrices es (x,y) = (0,0) 
+
+b)
+
+g1(x,y) = sen(y)
+g2(x,y) = -6x + y
+
+g1:
+    x = sen(y)
+
+g2:
+   y = -6x + y
+   0 = -6*sen(y)
+   0 = sen(y) => y = n*pi 
+
+Por lo tanto el punto fijo para estas generatrices es (x,y) = (0,n*pi) 
+
+
+%}
+
